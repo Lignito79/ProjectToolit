e43dd47f36451ac836ed51f423cf0a25c5777c69
Lignito79 <joseangelrenteria1159@gmail.com>
2023-04-18 23:34:23 -0600

    Polished Back-end

--- src/app/services/azure-devops-api.service.ts
+++ src/app/services/azure-devops-api.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { HttpBackend, HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, pairs, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { Buffer } from "buffer";
 import { BehaviorSubject } from 'rxjs/';
+import * as jsonpatch from 'jsonpatch';
+
 
 @Injectable({
   providedIn: 'root'
@@ -33,7 +35,7 @@ export class AzureDevopsAPIService {
     let pairs;
 
     try {
-
+      
       if (parsedLine.body.length == 0) {
         pairs = 1;
       } else if (parsedLine.link && parsedLine.body && Array.isArray(parsedLine.body)) {
@@ -44,51 +46,104 @@ export class AzureDevopsAPIService {
       console.error(`Error parsing JSON on line '${jsonLines}': ${error}`);
     }
     
-    this.makeRequest(parsedLine.type, parsedLine.link, pairs).subscribe((data: any) => {
+    this.makeRequest(parsedLine.type, parsedLine.ContentType, parsedLine.link, pairs).subscribe((data: any) => {
 	    console.log(data);
     });
 
     return parsedLine;
   }
 
-  makeRequest(typeOperation: string, link: string, bodyPairs): Observable<any> {
+  createBody(bodyPairs){
+    //SI FUNCIONA PARA CREAR UN PATCH
+    //let bodyString: string = "[ {"
+    let bodyString: string = "{"
+
+      for (let i = 0; i < bodyPairs.length; i++){
+        bodyString = bodyString + `"${bodyPairs[i].key}": "${bodyPairs[i].value}"`;
+
+        if (i == bodyPairs.length-1) {
+          //SI FUNCIONA PARA CREAR UN PATCH
+          //bodyString = bodyString + "} ]";
+          bodyString = bodyString + "}";
+        } else {
+          bodyString = bodyString + ", ";
+        }
+      }
+
+      return bodyString;
+  }
+
+  makeRequest(typeOperation: string, contentType: string, link: string, bodyPairs): Observable<any> {
+
     
-    const headers = new HttpHeaders({
-      'Authorization': 'Basic ' + Buffer.from(':' + process.env['NG_APP_TOK']).toString('base64')
-    });
-
-    // Solo se necesita un body en llamadas POST y no en GET.    
-
-    if(typeOperation == "POST"){    
-      
-      const bodyString = `{"${bodyPairs[0].key}": "${bodyPairs[0].value}"}`
+    // Solo se necesita un body en llamadas POST y PATCH, no en GET.
     
-      const body = JSON.parse(bodyString);
-      
-      return this.http.post(link, body, { headers: headers });
+    //------------------------------------------------------------------------------------------------
+    // MÉTODO GET
+    if (typeOperation == "GET"){
 
-    } else if (typeOperation == "GET"){
+      const headers = new HttpHeaders({
+        'Authorization': 'Basic ' + Buffer.from(':' + process.env['NG_APP_TOK']).toString('base64')
+      });
       
       return this.http.get(link, { headers: headers });
 
-    } else if (typeOperation == "PATCH"){
 
-      // Hacer un FOR que recorra el array de pares para crear un body de varias líneas o parámetros
-      const bodyString = `{"${bodyPairs[0].key}": "${bodyPairs[0].value}"}`
-    
+    //------------------------------------------------------------------------------------------------
+    // MÉTODO POST. Si en el comando proporcionado por el bot se especifica que el body debe ser un json patch, se llama la funcion de
+    // para crear uno. Si no, entonces se crea un "body" normal.
+    } if(typeOperation == "POST"){
+
+      // Se incluirá un content-type al header, y este será igual al valor de una variable con información que proporciona el bot.
+      let headers;
+      
+      // AQUÍ SE PONDRÁ UN IF. SI EL BOT DICE QUE EL CONTENT-TYPE ES JSON PATCH, SE CONVIERTE EL BODY OBTENIDO
+      // A UN DOCUMENTO JSON PATCH (LA CONVERSIÓN SE HARÁ SÓLO CON CONCATENACIÓN DE STRINGS :D).
+
+      let bodyString = this.createBody(bodyPairs);
+
+      if(contentType == "application/json-patch+json"){
+        bodyString = "[" + bodyString + "]";
+        headers = new HttpHeaders({
+          'Authorization': 'Basic ' + Buffer.from(':' + process.env['NG_APP_TOK']).toString('base64'),
+          'Content-Type': 'application/json-patch+json'
+        });
+      } else {
+        headers = new HttpHeaders({
+          'Authorization': 'Basic ' + Buffer.from(':' + process.env['NG_APP_TOK']).toString('base64')
+        });
+      }
+
       const body = JSON.parse(bodyString);
 
-      return this.http.patch(link, body, { headers: headers });
+      // Se crea un body a partir del string formado por los pares obtenidos y después de decidir si es un
+      // Json patch o no.
+      
+      console.log(body);
+      
+      return this.http.post(link, body, { headers: headers });
+
+
+    //------------------------------------------------------------------------------------------------
+    // MÉTODO PATCH. Por ahora es lo mismo que el POST.
+    } else if (typeOperation == "PATCH"){
+
+      const headers = new HttpHeaders({
+        'Authorization': 'Basic ' + Buffer.from(':' + process.env['NG_APP_TOK']).toString('base64')
+      });
+
+      // Se obtiene un json patch si es necesario
+      const body = this.createBody(bodyPairs);
+      
+      console.log(body);
+      
+      return this.http.post(link, body, { headers: headers });
 
     }
     
   }
   
 }
-  
-
-
-
 
 // Give me the work item from my Azure DevOps project with an ID of 47
     //{"type": "GET ", "link": "https://dev.azure.com/multiAgentes/MultiAgentesTC3004B.103/_apis/wit/workitems/47?api-version=7.0", "body": []}
